9bfa1fd8da4b0b3b5ee7d569413622478f35791d
BackThrowTheBaguetteBrawler <74450965+BackThrowTheBaguetteBrawler@users.noreply.github.com>
2021-01-25 14:17:20 -0500

    Add A button and Left Dpad To be used for click

--- HTML5/WiiUTablet.js
+++ HTML5/WiiUTablet.js
@@ -2,40 +2,46 @@ var touch=0;
 var tx=0;
 var ty=0;
 var socket;
-var host = "ws://192.168.1.2:8887";
+var host = "ws://10.42.0.1:8887";
 var packets=0;
+var aButton=0;
 
 window.onload = function () {
-	try{
-		socket = new WebSocket(host);
+        try{
+            	socket = new WebSocket(host);
 
-		socket.onopen = function(){
-		}
+                socket.onopen = function(){
+                }
 
-		socket.onmessage = function(msg){
-		}
+                socket.onmessage = function(msg){
+                }
 
-		socket.onclose = function(){
-		}			
-	} catch(exception){
-	}
-	setInterval("loop()",1);
+                socket.onclose = function(){
+                }
+        } catch(exception){
+        }
+        setInterval("loop()",1);
 }
 
 function loop(){ 
-	wiiu.gamepad.update();
-	if(wiiu.gamepad.isDataValid==1)
-	{
-		touch = wiiu.gamepad.tpTouch;
-		tx = wiiu.gamepad.tpX;
-		ty = wiiu.gamepad.tpY;
-	}
-	
-	socket.send(touch+ ";" +tx+ ";" +ty);
-	packets++;
-	updateS();
+        wiiu.gamepad.update();
+        if(wiiu.gamepad.isDataValid==1){
+                touch = wiiu.gamepad.tpTouch;
+                tx = wiiu.gamepad.tpX;
+                ty = wiiu.gamepad.tpY;
+        if(wiiu.gamepad.hold==32768 || wiiu.gamepad.hold==2048){
+        aButton=1;
+        }
+        else{
+	aButton=0;
+        }
+
+        socket.send(touch+ ";" +tx+ ";" +ty+ ";" +aButton);
+        packets++;
+        updateS();
 }
 
 function updateS(){
-	document.body.innerHTML = "Touch? = " +touch+ "</br>X = " +tx+ "</br>Y = " +ty+ "</br>Packets: " +packets;
+//      document.body.innerHTML = "Touch? = " +touch+ "</br>X = " +tx+ "</br>Y = " +ty+ "</br>Packets: " +packets+ "  </br>"+wiiu.g>
+}
 }
